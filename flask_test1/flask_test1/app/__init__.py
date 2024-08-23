80508850854503bc2c8799b68c25e20b4b5c5a4c
B0ond <amiram19@yandex.ru>
2024-08-23 15:38:16 +0300

    Broken, need to fix .env

--- flask_test1/flask_test1/app/__init__.py
+++ flask_test1/flask_test1/app/__init__.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+from sqlalchemy.testing.plugin.bootstrap import bootstrap_file
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
 
+    bootstrap_css = os.getenv('BOOTSTRAP_CSS')
+
 
     @app.route("/")
     def index():
-        return render_template('index.html')
+        return render_template('index.html', bootstrap_css=bootstrap_css)
 
     @app.route("/about")
     def about_us():
