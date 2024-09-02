f80e863402d381684e53bd4cf557d005925c5ab3
B0ond <amiram19@yandex.ru>
2024-09-02 16:26:09 +0300

    Normal, next BLUEPRINTS

--- flask_test1/flask_test1/app/__init__.py
+++ flask_test1/flask_test1/app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
-from .extentions import db
+from .config import Config
+
+from .extensions import db
 import os
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+    app.config.from_object(config_class)
     db.init_app(app)
+
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     with app.app_context():
         db.create_all()
     load_dotenv()
@@ -31,3 +35,10 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     app.run()
+
+
+
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+    # db.init_app(app)
+    # with app.app_context():
+    #     db.create_all()
