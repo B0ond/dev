bb40161c21145bf59c24e58f8b95bc890672a5c6
Gambit <amiram19@yandex.ru>
2024-09-08 20:01:18 +0300

    РАбота с шаблонами и связь с блупринт

--- flask_test1/flask_test1/app/__init__.py
+++ flask_test1/flask_test1/app/__init__.py
@@ -2,7 +2,10 @@ import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from .config import Config
-from .routes import user
+from .routes.rout_user import user
+from .routes.rout_post import post
+from .routes.rout_main import main
+from .routes.rout_about import about
 from .extensions import db
 
 
@@ -12,12 +15,16 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db.init_app(app)
     app.register_blueprint(user)
+    app.register_blueprint(post)
+    app.register_blueprint(main)
+    app.register_blueprint(about)
+
 
     # app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     with app.app_context():
         db.create_all()
-    load_dotenv()
 
+    load_dotenv()
     bootstrap_css = os.getenv('CSS_BOOTSTRAP')
 
     @app.context_processor
@@ -25,13 +32,14 @@ def create_app(config_class=Config):
         return dict(bootstrap_css=bootstrap_css)
 
 
-    @app.route("/")
-    def index():
-        return render_template('index.html')
+    # @app.route("/")
+    # def index():
+    #     return render_template('main/index.html')
 
-    @app.route("/about")
-    def about():
-        return render_template('about.html')
+    # @app.route("/about")
+    # def about():
+    #     return render_template('about/about.html')
+
     return app
 
 if __name__ == "__main__":
--- flask_test1/flask_test1/app/routes/rout_user.py
+++ flask_test1/flask_test1/app/routes/rout_user.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
-from ..models.models import User
-from .extensions import db
+from ..extensions import db
+from ..models.model_user import User
 
-user = Blueprint('user_bane', __name__)
+user = Blueprint('user', __name__)
 
 @user.route('/user/<name>')
 def create_user(name):
     user = User(name=name)
     db.session.add(user)
     db.session.commit()
-    return 'USer created successfully!'
+    return 'User created successfully!'
