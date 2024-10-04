cf5949ad3355df0d24cdbe97f870c0c521ab837b
B0ond <amiram19@yandex.ru>
2024-10-04 15:40:32 +0300

    add some libs

--- flask_test1/flask_test1/app/functions.py
+++ flask_test1/flask_test1/app/functions.py
@@ -1,5 +1,7 @@
 import os
 import secrets
+from tkinter import Image
+from PIL import Image
 
 from flask import current_app  # можем обращаться к конфигурации из разных частей приложения
 
@@ -8,4 +10,9 @@ def save_picture(picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(picture.filename)  #делит картинку на название (_,) и расширение (f_ext)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(current_app.config['SERVER_PATH'], picture_fn)
+    picture_path = os.path.join(current_app.config['SERVER_PATH'], picture_fn)  # создаем путь дял картинки
+    output_size = (125, 125)  # делаем рейсайз картинки
+    i = Image.open(picture)  # открываем картинку
+    i.thumbnail(output_size)  # обращаемся к картинке
+    i.save(picture_path)  # сохраняем по пути
+    return picture_fn  # необходимо для бд что бы достать картинку
