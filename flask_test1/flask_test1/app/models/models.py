21503de319e34da14b682e3147dcaa7fc13b9c5b
B0ond <amiram19@yandex.ru>
2024-10-08 09:53:48 +0300

    change directory

--- flask_test1/flask_test1/app/models/models.py
+++
@@ -1,7 +0,0 @@
-from ..extensions import db
-
-
-# пока просто лежит
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
--- flask_test1/flask_test1/app/routes/rout_about.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-about = Blueprint('about', __name__)
-
-
-# роут "о нас"
-@about.route('/about')
-def index():
-    return render_template('about/about.html')
